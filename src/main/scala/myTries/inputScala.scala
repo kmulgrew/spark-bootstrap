7de0af42c62777826509b2ed88a16c2a78db90f8
Rehan Mahmood <ksm348@nyu.edu>
2017-02-21 14:06:27 +0000

    fix my java program and fix corrupted git repository

--- src/main/scala/myTries/inputScala.scala
+++ src/main/scala/myTries/inputScala.scala
@@ -2,11 +2,15 @@ package myTries
 
 import org.apache.spark.sql.SparkSession
 
-object input {
+object inputScala {
   def main(args: Array[String]) {
+     val t0 = System.nanoTime()
+
+
+
     val spark = SparkSession
       .builder
-      .appName("input").master("local")
+      .appName("inputJava").master("local")
       .getOrCreate()
     val file = args(0)
     val input = spark.sparkContext.textFile(file)
@@ -14,5 +18,7 @@ object input {
     val words = input.flatMap(line => line.split(" "))
     val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
     counts.saveAsTextFile(output)
+     val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
   }
 }
