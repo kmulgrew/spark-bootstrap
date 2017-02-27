90e4cc7cdf623ba8ecdd8c2ef4200616433299ec
Rehan Mahmood <ksm348@nyu.edu>
2017-02-27 14:23:47 +0000

    sucessful authentication kerberos sort of, error remains

--- src/main/java/myTries/inputJava3.java
+++ src/main/java/myTries/inputJava3.java
@@ -15,15 +15,21 @@ import java.util.Arrays;
 /**
  * Created by andrew.addison on 2/23/17.
  */
+
+//arg 0 user
+   //arg 2 keytab
+   //arg3 fs.defaultDS
+   //arg 4 -> (args.length-1) folders to traverse
+   //arg args.length-1 regex for file names to match
 public class inputJava3
 {
    public static void main(String[] args) throws IOException
    {
       long startTime = System.nanoTime();
 
-      String[] inputFolderArray = new String[args.length - 3];
+      String[] inputFolderArray = new String[args.length - 4];
       int folderArrayIndex = 0;
-      for (int i = 2; i < args.length - 1; i++)
+      for (int i = 3; i < args.length - 1; i++)
       {
          //System.out.println(args[i]);
          inputFolderArray[folderArrayIndex++] = args[i];
@@ -34,11 +40,22 @@ public class inputJava3
       String master = "local[*]";
 
       SparkConf conf = new SparkConf().setAppName(WordCountTask.class.getName()).setMaster(master);
+
+      org.apache.hadoop.conf.Configuration conf2 = new org.apache.hadoop.conf.Configuration();
+
+
+
+      conf2.set("fs.defaultFS",args[2]);
+      conf2.set("hadoop.security.authentication", "kerberos");
       final String user = args[0];
       final String keyPath = args[1];
-
+      UserGroupInformation.setConfiguration(conf2);
       UserGroupInformation.loginUserFromKeytab(user, keyPath);
-      System.out.println(UserGroupInformation.isLoginKeytabBased() );
+      System.out.println(UserGroupInformation.getLoginUser().toString());
+      //System.out.println(UserGroupInformation.isLoginKeytabBased() );
+      //System.out.println(UserGroupInformation.getLoginUser().isFromKeytab());
+      System.out.println(UserGroupInformation.getCurrentUser());
+      System.out.println(UserGroupInformation.isSecurityEnabled());
       JavaSparkContext sc = new JavaSparkContext(conf);
       JavaRDD<String> input = sc.emptyRDD();
       for (int i = 0; i < inputFolderArray.length; i++)
@@ -46,6 +63,7 @@ public class inputJava3
          JavaRDD<String> input2 = sc.textFile(inputFolderArray[i] + regex); //"/[wkd]ing");
          input = input.union(input2);
       }
+
       JavaRDD<String> words = input.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
       words = words.repartition(100);
       JavaPairRDD<String, Integer> counts = words.mapToPair(new PairFunction<String, String, Integer>()
